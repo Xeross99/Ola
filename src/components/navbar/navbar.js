e6f74f611b3f377fd141774c190dd1eb3fce4f45
Xeross99 <m.krzysteczko@icloud.com>
2023-11-04 17:42:53 +0100

    hamburger nav animations

--- src/components/navbar/navbar.js
+++ src/components/navbar/navbar.js
@@ -6,19 +6,43 @@ const Navbar = () => {
   const genericHamburgerLine = `h-[2px] w-8 my-1 rounded-full bg-gray-50 transition ease transform duration-300`;
 
   return (
-    <div className="w-screen h-11 fixed top-0 bg-black/80 text-gray-50 font-light text-lg backdrop-blur-xl px-10 z-50">
-      <ul className='hidden sm:flex flex-row justify-end gap-10 p-2'>
-        <motion.li className='nav-animation'>O mnie</motion.li>
-        <motion.li className='nav-animation'>Zdrowie</motion.li>
-        <motion.li className='nav-animation'>Aktywność</motion.li>
-        <motion.li className='nav-animation'>Sklep</motion.li>
-      </ul>
+    <div className="w-screen h-11 fixed top-0 bg-black/80 text-gray-50 font-light text-lg z-50 overflow-hidden">
+      <nav className='backdrop-blur-xl w-full h-full'>
+        <ul className='hidden sm:flex flex-row justify-end gap-10 p-2 px-10'>
+          <motion.li className='nav-animation'>O mnie</motion.li>
+          <motion.li className='nav-animation'>Zdrowie</motion.li>
+          <motion.li className='nav-animation'>Aktywność</motion.li>
+          <motion.li className='nav-animation'>Sklep</motion.li>
+        </ul>
+      </nav>
 
-      <button onClick={() => setIsOpen(!isOpen)} className="flex sm:hidden flex-col h-fit w-fit justify-center group absolute right-10 top-2">
+      <button onClick={() => setIsOpen(!isOpen)} className="flex sm:hidden flex-col h-fit w-fit justify-center group absolute right-10 top-2 z-50 cursor-pointer">
         <div className={`${genericHamburgerLine} ${ isOpen ? "rotate-45 translate-y-[10px] opacity-100 group-hover:opacity-70" : "opacity-100 group-hover:opacity-70"}`} />
         <div className={`${genericHamburgerLine} ${ isOpen ? "opacity-0" : "opacity-100 group-hover:opacity-70"}`}/>
-        <div className={`${genericHamburgerLine} ${isOpen ? "-rotate-45 -translate-y-[10px] opacity-100 group-hover:opacity-70" : "opacity-100 group-hover:opacity-70"}`}/>
+        <div className={`${genericHamburgerLine} ${ isOpen ? "-rotate-45 -translate-y-[10px] opacity-100 group-hover:opacity-70" : "opacity-100 group-hover:opacity-70"}`}/>
       </button>
+
+      <div className={`block sm:hidden z-40 fixed bg-black/80 text-gray-50 font-light text-lg inset-0 transition ease transform duration-300 ${isOpen ? "translate-y-0" : "-translate-y-full blur-3xl "} `}>
+        <ul className='fixed text-5xl top-1/2 right-1/2 -translate-y-1/2 translate-x-1/2 flex flex-col gap-10 items-center'>
+            <motion.li  whileHover={{ scale: 1.2 }} 
+                        whileTap={{ scale: 0.8 }}
+                        style={{ x: 0 }}
+                        onClick={() => setIsOpen(false)} className='cursor-pointer'>O mnie</motion.li>
+            <motion.li whileHover={{ scale: 1.2 }}
+                        whileTap={{ scale: 0.8 }}
+                        style={{ x: 0 }} 
+                        onClick={() => setIsOpen(false)} className='cursor-pointer'>Zdrowie</motion.li>
+            <motion.li whileHover={{ scale: 1.2 }}
+                        whileTap={{ scale: 0.8 }}
+                        style={{ x: 0 }} 
+                        onClick={() => setIsOpen(false)} className='cursor-pointer'>Aktywność</motion.li>
+            <motion.li whileHover={{ scale: 1.2 }}
+                        whileTap={{ scale: 0.8 }}
+                        style={{ x: 0 }} 
+                        onClick={() => setIsOpen(false)} className='cursor-pointer'>Sklep</motion.li>
+          </ul>
+      </div>
+      
     </div>
   );
 }
